b5bd13b7b10de3be4d938dbb5518f7243d6acf72
Anjana <anjanaindu3699@gmail.com>
2026-03-26 20:30:20 +0530

    user

--- user-service/app/config.py
+++ user-service/app/config.py
@@ -1,3 +1,6 @@
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./user.db")
+DEFAULT_MONGODB_URL = "mongodb+srv://Admin:password@mtit.9eco5id.mongodb.net/restaurant?appName=mtit"
+
+MONGODB_URL = os.getenv("MONGODB_URL") or DEFAULT_MONGODB_URL
+MONGO_DB_NAME = os.getenv("MONGO_DB_NAME") or "restaurant"
